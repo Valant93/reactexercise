ed64fda0452fcf0a6cf8bc97f19d0af3b5eabfc1
Antonio <vallettaantonio82@gmail.com>
2024-06-17 16:12:10 +0200

    Login

---
+++ controlled-forms/src/Login.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false);
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleRememberChange = (e) => {
+    setRemember(e.target.checked);
+  };
+
+  const handleSubmit = () => {
+    console.log("Username:", username);
+    console.log("Password:", password);
+    console.log("Remember me:", remember);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Remember me:
+          <input
+            type="checkbox"
+            checked={remember}
+            onChange={handleRememberChange}
+          />
+        </label>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
